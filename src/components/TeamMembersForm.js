d63ea1a6c6d671e36e6898d74af5771da88a79da
Martin Brueggemann <marty.brueggemann@Martins-MacBook-Pro.local>
2021-06-06 13:51:04 -0500

    building TeamMembersForm

--- src/components/TeamMembersForm.js
+++ src/components/TeamMembersForm.js
@@ -1,1 +1,61 @@
 import React from 'react';
+
+function TeamMembersForm (props) {
+    const { data, update, build } = props
+
+    const changeData = event => {
+        const { name, value } = event.target
+        update(name, value)
+    }
+
+    const changeBuild = event => {
+        event.preventDefault()
+        build
+    }
+
+    return (
+        <form className='form' changeBuild={changeBuild}>
+            <div className='form entries'>
+                <label>Name
+                    <input
+                        type='text'
+                        name='name'
+                        changeData={changeData}
+                        data={data.name}
+                />
+                </label>
+
+                <label>Position
+                    <input
+                        type='text'
+                        name='position'
+                        changeData={changeData}
+                        data={data.position}
+                />
+                </label>
+
+                <label>Email
+                    <input
+                        type='email'
+                        name='email'
+                        changeData={changeData}
+                        data={data.email}
+                />
+                </label>
+
+                <label>Campus
+                    <select value={data.campus} name='campus' changeData={changeData}>
+                        <option value=''>--- select your campus ---</option>
+                        <option value='San Diego'>San Diego</option>
+                        <option value='Mpls'>Minneapolis</option>
+                    </select>
+                </label>
+
+                <div className='build'>
+                    <button disabled
+                </div>
+            </div>
+
+        </form>
+    )
+}
